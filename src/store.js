67f91fee74a50b1d3a0f67b0814198f6bf4a29ab
Arnab <arnab.ganguly@settlin.in>
2023-06-13 19:51:50 +0530

    fix: other changes

--- src/store.js
+++ src/store.js
@@ -1,14 +1,13 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
 import userReducer from './reducers/userReducer';
 import cartReducer from './reducers/cartReducer';
-
+import {sessionStorageMiddleware} from './middleware/sessionStorage';
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
-
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(sessionStorageMiddleware));
 
 export default store;
