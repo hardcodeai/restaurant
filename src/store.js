7d8e002a3f8221f5c38b4272661a4f8325957e7c
Arnab <arnab.ganguly@settlin.in>
2023-06-13 19:14:52 +0530

    fix: added changes

--- src/store.js
+++ src/store.js
@@ -2,13 +2,13 @@ import { createStore, combineReducers } from 'redux';
 import userReducer from './reducers/userReducer';
 import cartReducer from './reducers/cartReducer';
 
-// Combine multiple reducers into a single rootReducer
+
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
-// Create the Redux store
+
 const store = createStore(rootReducer);
 
 export default store;
